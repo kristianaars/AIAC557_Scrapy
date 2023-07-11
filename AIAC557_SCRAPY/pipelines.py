69e522fb39b4b5edc032deb16707f7581ddc138b
Kristian V. Aars <kristian@aars.me>
2023-07-11 23:45:25 +0200

    Implemented CSV pipeline

--- AIAC557_SCRAPY/pipelines.py
+++ AIAC557_SCRAPY/pipelines.py
@@ -6,8 +6,22 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exporters import CsvItemExporter
 
 
-class Aiac557ScrapyPipeline:
+class CsvPipeline:
+
+    def __init__(self):
+        self.file = open('products.csv', 'ab')
+        self.exporter = CsvItemExporter(self.file, include_headers_line=True)
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+
+    def spider_opened(self, spider):
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
--- AIAC557_SCRAPY/spiders/daraz_product_spider.py
+++ AIAC557_SCRAPY/spiders/daraz_product_spider.py
@@ -1,29 +1,48 @@
+from datetime import datetime
 from pathlib import Path
 
 import scrapy
 import chompjs
 
+
 class DarazProductSpider(scrapy.Spider):
     name = "daraz_products"
+    current_page = 1
+    baseurl = "https://www.daraz.com.np/mens-sneakers/caliber-shoes/"
 
     def start_requests(self):
-        urls = [
-            "https://www.daraz.com.np/mens-t-shirts/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url,
-                                 callback=self.parse,
-                                 meta={'playwright': True})
-
+        yield scrapy.Request(url=self.baseurl,
+                             callback=self.parse,
+                             meta={'playwright': True})
 
     def parse(self, response):
+        is_next_page = response.css('.ant-pagination-next::attr(aria-disabled)').get()
+
+        if is_next_page == 'false':
+            self.current_page += 1
+            yield response.follow("%s?page=%s" % (self.baseurl, str(self.current_page)),
+                                  self.parse,
+                                  meta={'playwright': True})
+
         item_links = response.css('.gridItem--Yd0sa div div div div a::attr(href)').getall()
         yield from response.follow_all(item_links,
                                        self.parseProduct,
                                        meta={'playwright': True})
 
     def parseProduct(self, response):
+        sku = ''
+
+        specifications = response.css('.pdp-general-features ul li')
+        for s in specifications:
+            spec_title = s.css('.key-title::text').get()
+            print("SPEC-TITLE:|%s|" % (spec_title))
+            if spec_title == ' SKU  ':
+                print("SPEC-VALUE: %s" % (s.css('div::text').get()))
+                sku = s.css('div::text').get()
+
         yield {
+            'datetime': datetime.now().isoformat(),
+            'sku': sku,
             'name': response.css('.pdp-mod-product-badge-title::text').get(),
             'brand': response.css('.pdp-product-brand__brand-link::text').get(),
             'price': response.css('.pdp-price_size_xl::text').get().replace('Rs. ', '').replace(',', ''),
